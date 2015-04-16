9f226ce6236578d76bd34750cb2c6a7d31d15dcd
Galaad Gauthier <coontail7@gmail.com>
2015-04-16 11:00:02 +0200

    Raise error issue

--- lib/overrides/string.rb
+++ lib/overrides/string.rb
@@ -1,7 +1,7 @@
 class String
   
   def ^(other)
-    raise ArgumentError, "Can't bitwise-XOR a String with a non-String"
+    raise ArgumentError, "Can't bitwise-XOR a String with a non-String" unless other.kind_of? String
     raise ArgumentError, "Can't bitwise-XOR strings of different length" unless self.length == other.length
     (0..self.length-1).collect{ |i| self[i].ord ^ other[i].ord }.pack("C*")
   end
