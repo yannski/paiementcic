26d383421a3ac5da9891031abcfa3789866e68a1
Galaad Gauthier <coontail7@gmail.com>
2015-04-27 11:20:50 +0200

    Fix

--- lib/paiement_cic/form_helper.rb
+++ lib/paiement_cic/form_helper.rb
@@ -1,5 +1,4 @@
 module PaiementCic::FormHelper
-  require 'open-uri'
 
   def paiement_cic_hidden_fields(reference, price, options = {})
     oMac = PaiementCic::TPE.new(options)
@@ -21,27 +20,6 @@ module PaiementCic::FormHelper
     end
 
     html
-  end
-
-  def paiement_cic_iframe_tag(reference, price, options = {})
-
-    oMac = PaiementCic::TPE.new(options)
-    oa = oMac.attributes(reference, price, options)
-    chaineMAC = oMac.compute_hmac_sha1(oa.values.join('*'))
-
-    url_retour      = options[:url_retour]
-    url_retour_ok   = options[:url_retour_ok]
-    url_retour_err  = options[:url_retour_err]
-
-    iframe_params = oa.attributes.merge({
-      mode_affichage: "iframe",
-      url_retour: options[:url_retour],
-      url_retour_err: options[:url_retour_err],
-      url_retour_ok: options[:url_retour_ok]
-    })
-
-    iframe_url = PaiementCic.default_config.target_url + "?" + iframe_params.map{|k,v| "#{k}=#{URI::encode(v)}"}.join("&")
-    content_tag :iframe, src: iframe_url
   end
 
 end
--- lib/paiement_cic/railtie.rb
+++ lib/paiement_cic/railtie.rb
@@ -4,6 +4,7 @@ module PaiementCic
   class Railtie < Rails::Railtie
     initializer "paiement_cic.form_helpers" do
       ActionView::Base.send :include, FormHelper
+      ActionView::Base.send :include, IframeHelper
     end
   end
 end
