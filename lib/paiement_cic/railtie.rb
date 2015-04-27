20da89e6d307a9ded3d5f049394afbe3474a2e4a
Galaad Gauthier <coontail7@gmail.com>
2015-04-27 11:25:06 +0200

    Fix

--- lib/paiement_cic/railtie.rb
+++ lib/paiement_cic/railtie.rb
@@ -2,7 +2,14 @@ require 'paiement_cic/form_helper'
 
 module PaiementCic
   class Railtie < Rails::Railtie
-    initializer "paiement_cic.form_helpers" { ActionView::Base.send :include, FormHelper }
-    initializer "paiement_cic.iframe_helpers" { ActionView::Base.send :include, IframeHelper }
+    
+    initializer "paiement_cic.form_helpers" do 
+      ActionView::Base.send :include, FormHelper
+    end
+
+    initializer "paiement_cic.iframe_helpers" do 
+      ActionView::Base.send :include, IframeHelper
+    end
+
   end
 end
